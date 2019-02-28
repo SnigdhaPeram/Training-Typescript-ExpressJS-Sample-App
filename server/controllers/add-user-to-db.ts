0bc2df1e16311f4d6436ec4300333fd3fb35c2da
Snigdha <snigdhaperam@gmail.com>
2019-02-28 12:35:27 -0500

    delete user from server

---
+++ server/controllers/add-user-to-db.ts
@@ -0,0 +1,11 @@
+import { db } from "../db";
+import { log } from "../logger";
+
+export const addUserToDb = (req: any, res: any) => {
+  const user = req.body;
+  log(`fetching user from request`, user);
+  log(`adding user to db`, user);
+  db.userList.push(user); // replace with mongodb.
+  log(`sending add user to client`, user);
+  res.json(user);
+};
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,23 @@
-import * as express from 'express';
-const { PORT } = require('./server-port');
+import * as express from "express";
+import { userRoutes } from "./routes/user-route";
 
+
+import * as bodyParser from "body-parser";
+const { PORT } = require("./server-port");
 const app = express();
 
+ //require to allow other port numer to make http calls
+app.use((req: any, res: any, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+ //body parser will help to read the body from request message and put it in express request body object.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+userRoutes(app);
 app
-  .get('/', (req: any, res: any) =>
-    res.send(`Node and express server is running on port ${PORT}`),
+  .get("/", (req: any, res: any) =>
+    res.send(`Node and express server is running on port ${PORT}`)
   )
   .listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
